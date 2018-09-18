bd5055a17aeb757dab2a330ffc55666484ec5074
Gina Fedder <ginafedder@yahoo.com>
2018-09-18 16:16:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(collection)
   i = 0 
   new_collection = []
   while i < collection.length 
-  i = i + 1 
-  yield(collection[i])
-  
+    new_collection << yield(collection[i])
+    i = i + 1 
   end
+  new_collection
 end
 
